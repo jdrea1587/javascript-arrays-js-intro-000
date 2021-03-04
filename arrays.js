97c3dbef7fe882d41626bd9e1885d9116279d34a
Joshua Rea <joshdanielrea@gmail.com>
2021-03-04 16:08:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,24 @@
 
 var chocolateBars ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array,element){
+var newArray = array;
+newArray.unshift(element);
+return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element);
+  return newArray;
+}
+
+function addElementToEndOfArray(array, element){
+var newArray = array;
+newArray.push(element);
+return newArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return newArray;
+}
